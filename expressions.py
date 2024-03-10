4715e7ac4a339c2284f791eb473511299984b5dd
McOwska <zuza.m.makowska@gmail.com>
2024-03-10 16:50:05 +0100

    add expressions list

---
+++ expressions.py
@@ -0,0 +1,65 @@
+# https://www.youtube.com/@migam org
+
+lesson_1 = [
+    'CZEŚĆ',  
+    'DZIEŃ DOBRY',
+    'DOBRY WIECZÓR',
+    'JAK SIĘ CZUJESZ',
+    'JA DOBRZE, A TY',
+    'BARDZO DOBRZE',
+    'DOBRZE',
+    'DO WIDZENIA',
+    'DOBRANOC',
+    'DO ZOBACZENIA'
+]
+
+lesson_2 = [
+    'JA', 
+    'TY', 
+    'ON/ONA', 
+    'ONI', 
+    'MY', 
+    'MAM', 
+    'NAZWISKO', 
+    'IMIĘ', 
+    'MIGOWY ZNAK',
+    'LAT/ WIEK', 
+    'GŁUCHY', 
+    'SŁYSZĄCY', 
+    'MASZ',
+]
+
+lesson_3 = [
+    'JEST', 
+    'ŻONA', 
+    'POZNAĆ', 
+    'TWÓJ/SWÓJ', 
+    'DZIĘKOWAĆ', 
+    'MĘŻCZYZNA', 
+    'PRZYJACIEL', 
+    'DZIECI', 
+    'ROZMAWIAĆ,' 
+    'KOBIETA', 
+    'WAS', 
+    'JEGO/JEJ', 
+    'PRZEPRASZAĆ', 
+    'PANI', 
+    'SZKOŁA', 
+    'MĄŻ', 
+    'PAN', 
+    'MÓJ/SWÓJ'
+]
+
+lesson_4 = [
+    'OSOBA',
+    'NUMER',
+    'LUDZIE',
+    'TELEFON',
+    'ZAWÓD',
+    'KOMÓRKA',
+    'WIEK',
+    'KONTO',
+    'ADRES',
+    'DOWÓD OSOBISTY',
+    'OBYWATELSTWO'    
+]
