43162e1deb0e86def107b3818dec7d7125131b97
McOwska <zuza.m.makowska@gmail.com>
2024-03-28 15:06:01 +0100

    update - data collection

--- data_collection.py
+++ data_collection.py
@@ -6,10 +6,18 @@ from itertools import product
 import keyboard
 from expressions import lesson_1, lesson_2, lesson_3, lesson_4
 
-# Create directories to save the recorded training data
-def prepare_dataset_directory(actions, sequences, path):
-    for action, sequence in product(actions, range(sequences)):
-        os.makedirs(os.path.join(path, action, str(sequence)), exist_ok=True)
+def create_directory(sequence, action):
+    #  check if sequence directory exists
+    if not os.path.exists(os.path.join("data/", action)):
+        os.makedirs(os.path.join("data/", action))
+    
+    #  check how many sequences are in the directory
+    sequence_list = os.listdir(os.path.join("data/", action))
+    sequences_no = len(sequence_list)
+    
+    # create a directory for the sequence
+    os.makedirs(os.path.join("data/", action, str(sequences_no)), exist_ok=True)
+    
 
 # Extracting keypoints and saving them to a file
 def process_image_and_extract_keypoints(cap, holistic, action, sequence, frame, path):
@@ -29,11 +37,10 @@ def process_image_and_extract_keypoints(cap, holistic, action, sequence, frame, 
     mp.solutions.drawing_utils.draw_landmarks(image, results.left_hand_landmarks, mp.solutions.holistic.HAND_CONNECTIONS)
     mp.solutions.drawing_utils.draw_landmarks(image, results.right_hand_landmarks, mp.solutions.holistic.HAND_CONNECTIONS)
 
-    # saving keypoints
     if frame is not None:
         keypoints = extract_keypoints(results)
         np.save(os.path.join(path, action, str(sequence), str(frame)), keypoints)
-
+    
     return image
 
 # Extracting keypoints
@@ -55,7 +62,7 @@ def main():
     frames = 30
     PATH = 'data'
 
-    prepare_dataset_directory(actions, sequences, PATH)
+    # prepare_dataset_directory(actions, sequences, PATH)
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         print("Cannot access camera.")
@@ -64,29 +71,37 @@ def main():
     with mp.solutions.holistic.Holistic(min_detection_confidence=0.75, min_tracking_confidence=0.75) as holistic:
         for action in actions:
             flag = 0
+            keypoints_sequence = []
             for sequence in range(sequences):
-                print(f"Collecting data for: {action} sequence no: {sequence}.")
+                # to start recording press space
+                print(f"Collecting data for: {action}.")
+                print("Press space to start recording, right arrow to skip to the next action, ESC to exit.")
+                while not keyboard.is_pressed(' '):
+                    image= process_image_and_extract_keypoints(cap, holistic, action, sequence, None, PATH)
+                    cv2.imshow('Camera', image)
+                    if cv2.waitKey(1) & 0xFF == 27:  # ESC key to exit
+                        cap.release()
+                        cv2.destroyAllWindows()
+                        return
+                    if keyboard.is_pressed('right'):
+                        flag += 1
+                        break
                 if flag != 0:
                     break
+                create_directory(sequence, action)
+                print(f"Collection data for: {action} sequence no: {sequence}.")
                 for frame in range(frames):
-                    if frame == 0:
-                        while not keyboard.is_pressed(' '):
-                            image = process_image_and_extract_keypoints(cap, holistic, action, sequence, None, PATH)
-                            cv2.imshow('Camera', image)
-                            if cv2.waitKey(1) & 0xFF == 27:  # ESC key to exit
-                                cap.release()
-                                cv2.destroyAllWindows()
-                                return
-                            if keyboard.is_pressed('right'):
-                                flag += 1
-                                break
-                    if flag != 0:
-                        break
                     print(f"Collecting frames. Action: {action} frame no: {frame}")
                     image = process_image_and_extract_keypoints(cap, holistic, action, sequence, frame, PATH)
                     cv2.imshow('Camera', image)
                     if cv2.waitKey(1) & 0xFF == 27:
                         break
+                if cv2.waitKey(1) & 0xFF == 27:
+                    break
+
+           
+                
+                
 
     cap.release()
     cv2.destroyAllWindows()
